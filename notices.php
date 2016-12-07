b5d94b2d473a7e8047a1fbb93bce3e6f98f29ae5
AmitD26 <amitdharmadhikari41@gmail.com>
2016-12-07 16:40:49 +0530

    Send notices.

---
+++ notices.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 7/12/16
+ * Time: 3:48 PM
+ */
+
+$host = 'localhost';
+$username = 'root';
+$password = 'Aaaa1234';
+$db_name = 'Dhareshwar';
+
+$conn = new mysqli($host,$username,$password,$db_name);
+if($conn->connect_error) {
+    die("Error in connecting to database.".$conn->connect_error);
+}
+
+$query = 'SELECT * FROM notices';
+$result = $conn->query($query);
+
+$data = new stdClass();
+while ($row = $result->fetch_assoc()) {
+    $notice_id = $row['notice_id'];
+    unset($row['notice_id']);
+    $data->$notice_id = $row;
+}
+echo json_encode($data);
